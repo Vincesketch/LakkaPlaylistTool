a78455ba6b6ce34b21211c6dc7d5516c9e05bb09
jinneec <lvjm@163.com>
2017-06-13 16:35:17 +0800

    push LakkaPlaylistTool project.

--- LakkaPlaylistTool/Program.cs
+++ LakkaPlaylistTool/Program.cs
@@ -16,7 +16,7 @@ namespace LakkaPlaylistTool
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMain());
+            Application.Run(new FrmLakka());
         }
     }
 }
